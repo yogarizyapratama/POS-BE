f725f3de6a4fce30969811487a6030c08a8c97c7
Yoga Rizya Pratama <yoga.pratama@assist.id>
2025-02-09 16:25:38 +0700

    add vercel json

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logging_1 = require("./application/logging");
-const web_1 = require("./application/web");
-web_1.web.listen(process.env.PORT || 7001, () => {
-    logging_1.logger.info(`Listening on port ${process.env.PORT || 7001}`);
-});
--- dist/model/user-model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toUserResponse = toUserResponse;
-function toUserResponse(user) {
-    return {
-        name: user.name,
-        username: user.username
-    };
-}
